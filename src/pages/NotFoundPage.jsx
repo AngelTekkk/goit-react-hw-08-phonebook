2ddcf28ee193413a2e90be61ff530edabd5d7df5
Andriy Makarov <angeltek69@gmail.com>
2022-09-07 11:50:35 +0200

    Fullfield

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,11 +2,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function NotFoundPage() {
   const navigate = useNavigate();
-  const location = useLocation();
-  console.log(location);
-  setTimeout(() => {
-    navigate('/');
-  }, 2000);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-white py-4">
       <div className="flex flex-col">
@@ -21,6 +17,14 @@ export default function NotFoundPage() {
             The page you are looking for could not be found.
           </div>
         </div>
+
+        <button
+          type="button"
+          className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+          onClick={() => navigate('/')}
+        >
+          Go back
+        </button>
       </div>
     </div>
   );
